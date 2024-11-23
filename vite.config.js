1c6c508ad0bf2cdc8276edf61749d2f12dbf736c
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2024-11-23 17:54:32 +0530

    vite config edited

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',    // This binds the app to all network interfaces, making it externally accessible
+    port: 5173,         // Specify the port you'd like to use
+  },
   plugins: [react()],
   resolve: {
     alias: {
