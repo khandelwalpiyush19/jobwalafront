2e76cf71ff1b52b178b7028a92580c5ca328f781
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2024-11-23 21:35:29 +0530

    xaxs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    minify: 'esbuild',  // Use esbuild for minification in production
+    sourcemap: false,   // Disable sourcemaps for production (optional)
+  },
+  define: {
+    'process.env.NODE_ENV': '"production"',  // Ensure production mode
+  },
   server: {
     host: '0.0.0.0',    // This binds the app to all network interfaces, making it externally accessible
     port: 5173,         // Specify the port you'd like to use
