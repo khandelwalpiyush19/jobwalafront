fddace506b1ff3350da2ab3b576802a149b69eff
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2024-11-23 18:46:07 +0530

    fefd

--- src/hooks/useGetAllJobs.jsx
+++ src/hooks/useGetAllJobs.jsx
@@ -1,25 +1,29 @@
-import { setAllJobs } from '@/redux/jobSlice'
-import { JOB_API_END_POINT } from '@/utils/constant'
-import axios from 'axios'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { setAllJobs } from '@/redux/jobSlice';
+import { JOB_API_END_POINT } from '@/utils/constant';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const useGetAllJobs = () => {
     const dispatch = useDispatch();
-    const {searchedQuery} = useSelector(store=>store.job);
-    useEffect(()=>{
+    const { searchedQuery } = useSelector(store => store.job);
+
+    useEffect(() => {
         const fetchAllJobs = async () => {
             try {
-                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,{withCredentials:true});
-                if(res.data.success){
+                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`, { withCredentials: true });
+                if (res.data.success) {
                     dispatch(setAllJobs(res.data.jobs));
                 }
             } catch (error) {
-                console.log(error);
+                console.error("Error fetching jobs:", error.response ? error.response.data : error.message);
             }
-        }
-        fetchAllJobs();
-    },[])
-}
+        };
 
-export default useGetAllJobs
+        if (searchedQuery) {
+            fetchAllJobs();
+        }
+    }, [searchedQuery]); // Depend on searchedQuery to refetch when it changes
+};
+
+export default useGetAllJobs;
